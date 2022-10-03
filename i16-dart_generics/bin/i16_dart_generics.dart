6651521c8f26802d1b73f899748290efd359c11e
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-03 13:17:15 +0100

    Creating a Turple structure with Generics#

--- i16-dart_generics/bin/i16_dart_generics.dart
+++ i16-dart_generics/bin/i16_dart_generics.dart
@@ -1,5 +1,35 @@
-import 'package:i16_dart_generics/i16_dart_generics.dart' as i16_dart_generics;
+library generic_programming;
 
 void main(List<String> arguments) {
-  print('Hello world: ${i16_dart_generics.calculate()}!');
+  Turple turple = Turple.fromList([3, 7, 10]);
+  print(turple.third);
+}
+
+//! Generic Type naming convesion ->
+abstract class Vector<E> {} //? <E> -> Element
+
+abstract class Map<K, V> {} //? <K, V> -> Key & Value
+
+Vector<R> cast<R>() => [] as Vector<R>; //? <R> -> Return
+
+List<T> castFrom<S, T>(List<S> sourcde) => []; //? <S, T> -> Source & Type
+
+abstract class Bloc<Event, State> {} //? You can also use any name.
+//! -->
+
+class Turple {
+  final int? _a;
+  final int? _b;
+  final int? _c;
+
+  const Turple(this._a, this._b, this._c);
+
+  Turple.fromList(List<int> list)
+      : _a = list.asMap().containsKey(0) ? list[0] : null,
+        _b = list.asMap().containsKey(1) ? list[1] : null,
+        _c = list.asMap().containsKey(2) ? list[2] : null;
+
+  int? get first => _a;
+  int? get second => _b;
+  int? get third => _c;
 }
--- i16-dart_generics/test/i16_dart_generics_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:i16_dart_generics/i16_dart_generics.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

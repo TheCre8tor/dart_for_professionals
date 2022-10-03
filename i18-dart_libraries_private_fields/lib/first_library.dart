1cf2361dcaa57fb4aaa67ef2521b001536fbb966
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-03 16:40:23 +0100

    Linking multiple libraries together

---
+++ i18-dart_libraries_private_fields/lib/first_library.dart
@@ -0,0 +1,26 @@
+library first_library;
+
+part 'second_library.dart';
+
+//! In Dart 1 feature should be placed in a seperate library,
+//! that's the reason why we linked two libraries together.
+
+class A {
+  final int _privateField = 5;
+  void _privateMethod() {}
+
+  final int publicField = 8;
+  void publicMethod() {}
+}
+
+void randomFunction() {
+  var a = A();
+
+  /* NOTE:
+     ! The reason why we were able to access private fields in the same
+     ! library is because Dart does not support Class Base private fields,
+     ! it only support Library base private fields.
+      */
+  a._privateField;
+  a._privateMethod();
+}
---
+++ i18-dart_libraries_private_fields/lib/second_library.dart
@@ -0,0 +1,7 @@
+part of 'first_library.dart';
+
+void anotherFunction() {
+  var actual = A();
+  actual._privateField;
+  actual._privateMethod();
+}

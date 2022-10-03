e039eeae40dd8e22d0d8d6f54a03737c35d03ae8
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-03 12:29:04 +0100

    Initial commit on Dart Generics

---
+++ i16-dart_generics/bin/i16_dart_generics.dart
@@ -0,0 +1,5 @@
+import 'package:i16_dart_generics/i16_dart_generics.dart' as i16_dart_generics;
+
+void main(List<String> arguments) {
+  print('Hello world: ${i16_dart_generics.calculate()}!');
+}
---
+++ i16-dart_generics/test/i16_dart_generics_test.dart
@@ -0,0 +1,8 @@
+import 'package:i16_dart_generics/i16_dart_generics.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
